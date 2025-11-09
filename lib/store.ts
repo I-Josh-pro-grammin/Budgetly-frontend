85fa15429cacce50ab588eca9357085d308a9488
IZERE JOSHUA <izerejoshua94@gmail.com>
2025-11-09 09:13:44 +0200

    done solving errors in frontend

--- lib/store.ts
+++ lib/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
-import budgetReducer from './slices/budgetSlice';
+// import budgetReducer from './slices/budgetSlice';
 import expenseReducer from './slices/expenseSlice';
 import categoryReducer from './slices/categorySlice';
 import templateReducer from './slices/templateSlice';
@@ -9,7 +9,7 @@ import chatbotReducer from './slices/chatbotSlice';
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    budget: budgetReducer,
+    // budget: budgetReducer,
     expense: expenseReducer,
     category: categoryReducer,
     template: templateReducer,
